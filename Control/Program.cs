0e6adc834797c410111c2fd33466cde067263901
ImaxGoga <45959194+ImaxGoga@users.noreply.github.com>
2022-09-20 17:15:59 +0300

    version 0.01

---
+++ Control/Program.cs
@@ -0,0 +1,18 @@
+﻿void foo()
+{
+    string[] words = new string[4];
+    for (int i = 0; i < words.GetLength(0); i++)
+    {
+        Console.WriteLine("Введите " + i + " слово: ");
+        words[i] = Console.ReadLine();
+    }
+    Console.Write("Ваш массив [");
+    for (int i = 0; i < words.GetLength(0); i++)
+    {
+        if(i != words.GetLength(0) -1)
+        Console.Write(words[i] + ", ");
+        else Console.WriteLine(words[i] + "]");
+    }
+}
+
+foo();
